148682c04b04e27eca6639ff6402980ebe311523
Kayla <kbaistrocchi@gmail.com>
2020-03-27 11:33:52 -0400

    lesson: basic functional component that receives props as argument

---
+++ src/components/card-list/card-list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card-list.css';
+import { Card } from '../card/card';
+
+// CardList will be a functional component
+export const CardList = (props) => {
+    console.log(props);
+    return (
+        <div className="card-list">
+            {/* don't need to use this keyword because props are passed in as paramenters */}
+            {
+            props.monsters.map(monster => <Card key={monster.id} monster={monster} />)
+            }   
+        </div>
+
+    )
+}
