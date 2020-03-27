d675ebd999e0e3032434ba0b6650ba152ec9de81
Kayla <kbaistrocchi@gmail.com>
2020-03-27 19:07:33 -0400

    Updates

---
+++ precache-manifest.8df1aee2564456660f44b2e2aabb1505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995cb8dda35bb9b17e11f455dcbf0db6",
+    "url": "/udemy-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4f9ad23f463f8b4ae273",
+    "url": "/udemy-monsters-rolodex/static/css/main.cba823ab.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/udemy-monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9ad23f463f8b4ae273",
+    "url": "/udemy-monsters-rolodex/static/js/main.6f0c0fee.chunk.js"
+  },
+  {
+    "revision": "43a3739419ab7c97f80d",
+    "url": "/udemy-monsters-rolodex/static/js/runtime-main.144d7d56.js"
+  }
+]);
